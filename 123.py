03d5c20e7a2b3999e20b93638d55e29c7c14c72c
Hans Palacios <architect.hp@gmail.com>
2019-04-23 10:56:13 -0500

    Include new RS light dome and RS light in obj network, and new RS mat in shop network set to plastic as default material.  Confirmed that they both process correctly on Houdini startup.  Add colors to nodes and turn of selectable toggle on nodes.

--- 123.py
+++ 123.py
@@ -11,33 +11,67 @@ def create_geoNode():
     # Create geo node
     geo = obj.createNode("geo", node_name="newGeo")
 
+    # Turn off geo node select flag
+    geo.setSelectableInViewport(False)
+
+    # Set color for node to dark green
+    geo.setColor(hou.Color(0.302, 0.525, 0.114))
+
+
+#########################################################
+# Function to create Redshift light dome & single light nodes
+def create_lightNodes():
+
+    # Access obj network
+    obj = hou.node("/obj")
+
+    #########################################
+    # Create Redshift light dome node
+    rslightdome = obj.createNode("rslightdome", node_name="rsLightDome")
+
+    #########################################
+    # Create Redshift light node
+    rslight = obj.createNode("rslight", node_name="rsLight")
+
+    # Move them so that they don't overlap other nodes
+    rslightdome.moveToGoodPosition()
+    rslight.moveToGoodPosition()
+
+    # Turn off light nodes display & select flags
+    rslightdome.setDisplayFlag(False)
+    rslight.setDisplayFlag(False)
+    rslightdome.setSelectableInViewport(False)
+    rslight.setSelectableInViewport(False)
+
+    # Set color for nodes to dark yellow
+    rslightdome.setColor(hou.Color(1.0, 0.725, 0.0))
+    rslight.setColor(hou.Color(1.0, 0.725, 0.0))
+
 
 #########################################################
 # Function to create camera node
 def create_cameraNode():
 
     # Access obj network
-    camObj = hou.node("/obj")
+    obj = hou.node("/obj")
 
     # Create cam node in obj network
-    cam = camObj.createNode("cam", "cam_1080")
+    cam = obj.createNode("cam", "cam_1080")
 
     # Also, move it so that it doesn't overlap other nodes
     cam.moveToGoodPosition()
-
-    # The method is separated from previous var initialization
-    # otherwise it will not retain its type of cam.  Basically,
-    # the following will not work:
-    # cam = camObj.createNode("cam", "cam_1080").moveToGoodPosition()
-    # since the result is no longer a cam because of the end function
 
     # Define resolution using dictionary format
     res = {'resx': 1920, 'resy': 1080}
     # Apply resolution to camera
     cam.setParms(res)
 
-    # Turn off camera node display flag
+    # Turn off camera node display & select flags
     cam.setDisplayFlag(False)
+    cam.setSelectableInViewport(False)
+
+    # Set color for node to dark blue
+    cam.setColor(hou.Color(0.094, 0.369, 0.69))
 
 
 #########################################################
@@ -60,13 +94,34 @@ def create_redshiftNode():
     # Also, move it so that it doesn't overlap other nodes
     redIPR.moveToGoodPosition()
 
+
+#########################################################
+# Function to create blank geo node
+def create_matNode():
+
+    # Access obj network
+    shop = hou.node("/shop")
+
+    # Create geo node
+    rsmat = shop.createNode("RS_Material", node_name="rsMat")
+
+    # Set color for node to dark red
+    rsmat.setColor(hou.Color(0.8, 0.016, 0.016))
+
+    # Set mat node to the preset of Plastic, which has a
+    # token of 2, hence the option identified
+    rsmat.setParms({"preset": "2"})
+
+
 #########################################################
 # Collect functions to generate all new nodes at startup
 # in new main() function
 def main():
     create_geoNode()
+    create_lightNodes()
     create_cameraNode()
     create_redshiftNode()
+    create_matNode()
 
 #########################################################
 # Call main function
--- 456.py
+++ 456.py
@@ -11,6 +11,42 @@ def create_geoNode():
     # Create geo node
     geo = obj.createNode("geo", node_name="newGeo")
 
+    # Turn off geo node select flag
+    geo.setSelectableInViewport(False)
+
+    # Set color for node to dark green
+    geo.setColor(hou.Color(0.302, 0.525, 0.114))
+
+
+#########################################################
+# Function to create Redshift light dome & single light nodes
+def create_lightNodes():
+
+    # Access obj network
+    obj = hou.node("/obj")
+
+    #########################################
+    # Create Redshift light dome node
+    rslightdome = obj.createNode("rslightdome", node_name="rsLightDome")
+
+    #########################################
+    # Create Redshift light node
+    rslight = obj.createNode("rslight", node_name="rsLight")
+
+    # Move them so that they don't overlap other nodes
+    rslightdome.moveToGoodPosition()
+    rslight.moveToGoodPosition()
+
+    # Turn off light nodes display & select flags
+    rslightdome.setDisplayFlag(False)
+    rslight.setDisplayFlag(False)
+    rslightdome.setSelectableInViewport(False)
+    rslight.setSelectableInViewport(False)
+
+    # Set color for nodes to dark yellow
+    rslightdome.setColor(hou.Color(1.0, 0.725, 0.0))
+    rslight.setColor(hou.Color(1.0, 0.725, 0.0))
+
 
 #########################################################
 # Function to create camera node
@@ -30,8 +66,12 @@ def create_cameraNode():
     # Apply resolution to camera
     cam.setParms(res)
 
-    # Turn off camera node display flag
+    # Turn off camera node display & select flags
     cam.setDisplayFlag(False)
+    cam.setSelectableInViewport(False)
+
+    # Set color for node to dark blue
+    cam.setColor(hou.Color(0.094, 0.369, 0.69))
 
 
 #########################################################
@@ -54,20 +94,79 @@ def create_redshiftNode():
     # Also, move it so that it doesn't overlap other nodes
     redIPR.moveToGoodPosition()
 
+
+#########################################################
+# Function to create blank geo node
+def create_matNode():
+
+    # Access obj network
+    shop = hou.node("/shop")
+
+    # Create geo node
+    rsmat = shop.createNode("RS_Material", node_name="rsMat")
+
+    # Set color for node to dark red
+    rsmat.setColor(hou.Color(0.8, 0.016, 0.016))
+
+    # Set mat node to the preset of Plastic, which has a
+    # token of 2, hence the option identified
+    rsmat.setParms({"preset": "2"})
+
+
 #########################################################
 # Collect functions to generate all new nodes at startup
 # in new main() function
 def main():
-
+    #########################################
     # Collect all nodes in obj network and returns them
     # in the form of a tuple (list)
-    nodes = hou.node('/obj').glob('*')
+    geos = hou.node('/obj').glob('*')
+    # Declare/initialize geo_exists as false
+    geo_exists = False
+
+    # Check each node in the obj network to see if the
+    # type matches that of a geo node
+    for node in geos:
+        if node.type().name() == 'geo':
+            # If there is a match, switch geo_exists and
+            # exit loop
+            geo_exists = True
+            break
+
+    if not geo_exists:
+        create_geoNode()
+
+
+    #########################################
+    # Collect all nodes in obj network and returns them
+    # in the form of a tuple (list)
+    lights = hou.node('/obj').glob('*')
+    # Declare/initialize light_exists as false
+    light_exists = False
+
+    # Check each node in the obj network to see if the
+    # type matches that of a light node
+    for node in lights:
+        if node.type().name() == 'rslightdome':
+            # If there is a match, switch light_exists and
+            # exit loop
+            light_exists = True
+            break
+
+    if not light_exists:
+        create_lightNodes()
+
+
+    #########################################
+    # Collect all nodes in obj network and returns them
+    # in the form of a tuple (list)
+    cams = hou.node('/obj').glob('*')
     # Declare/initialize cam_exists as false
     cam_exists = False
 
     # Check each node in the obj network to see if the
-    # type matches that of a camera
-    for node in nodes:
+    # type matches that of a camera node
+    for node in cams:
         if node.type().name() == 'cam':
             # If there is a match, switch cam_exists and
             # exit loop
@@ -77,6 +176,7 @@ def main():
     if not cam_exists:
         create_cameraNode()
 
+
     #########################################
     # Collect all nodes in out network and returns them
     # in the form of a tuple (list)
@@ -85,7 +185,7 @@ def main():
     rop_exists = False
 
     # Check each node in the out network to see if the
-    # type matches that of a Redshift ROP
+    # type matches that of a Redshift ROP node
     for node in rops:
         if node.type().name() == 'Redshift_ROP':
             # If there is a match, switch rop_exists and
@@ -100,6 +200,27 @@ def main():
     # If Redshift could not be created, deliver this message to UI
     except:
         hou.ui.displayMessage("An error occurred and Redshift ROP could not be created.")
+
+
+    #########################################
+    # Collect all nodes in shop network and returns them
+    # in the form of a tuple (list)
+    mats = hou.node('/shop').glob('*')
+    # Declare/initialize mat_exists as false
+    mat_exists = False
+
+    # Check each node in the obj network to see if the
+    # type matches that of an RS material node
+    for node in mats:
+        if node.type().name() == 'RS_Material':
+            # If there is a match, switch mat_exists and
+            # exit loop
+            mat_exists = True
+            break
+
+    if not mat_exists:
+        create_matNode()
+
 
 #########################################################
 # Call main function
