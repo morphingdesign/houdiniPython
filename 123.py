ba7e120a95cf9c3cf8b4a0b600be943313d840ce
Hans Palacios <architect.hp@gmail.com>
2019-08-09 23:41:43 -0500

    Refine startup script with network boxes, materials (point Cd), material assignments, and render settings.

--- 123.py
+++ 123.py
@@ -3,134 +3,26 @@
 
 #########################################################
 # Function to create blank geo node
-def create_geoNode():
+def create_geoNodeNet():
 
     # Access obj network
     obj = hou.node("/obj")
 
     # Create geo node
-    geo = obj.createNode("geo", node_name="geo_newGeo")
+    geo_new_node = obj.createNode("geo", node_name="geo_newGeo")
 
     # Turn off geo node select flag
-    geo.setSelectableInViewport(False)
+    geo_new_node.setSelectableInViewport(False)
 
     # Set color for node to dark green
-    geo.setColor(hou.Color(0.302, 0.525, 0.114))
-
-
-#########################################################
-# Function to create Redshift light dome & single light nodes
-def create_lightNodes():
-
-    # Access obj network
-    obj = hou.node("/obj")
-
-    #########################################
-    # Create Redshift light dome node
-    rslightdome = obj.createNode("rslightdome", node_name="rsLightDome")
-
-    #########################################
-    # Create Redshift area light node
-    rslight = obj.createNode("rslight", node_name="rsLight")
+    geo_new_node.setColor(hou.Color(0.302, 0.525, 0.114))
 
     #########################################
     # Set parameters
-    # Disable area light
-    rslight.setParms({"light_enabled": "0"})
-    # Add path to default Houdini HDRI in light dome
-    rslightdome.setParms({"env_map": "$HFS/houdini/pic/hdri/HDRIHaven_kiara_5_noon_2k.rat"})
-    # Disable HDRI background in light dome
-    rslightdome.setParms({"background_enable": "0"})
+    # Set material to reflect material
+    geo_new_node.setParms({"shop_materialpath": "/shop/rsMat_Reflect"})
 
     #########################################
-
-    # Turn off light nodes display & select flags
-    rslightdome.setDisplayFlag(False)
-    rslight.setDisplayFlag(False)
-    rslightdome.setSelectableInViewport(False)
-    rslight.setSelectableInViewport(False)
-
-    # Set color for nodes to dark yellow
-    rslightdome.setColor(hou.Color(1.0, 0.725, 0.0))
-    rslight.setColor(hou.Color(1.0, 0.725, 0.0))
-
-    # Move them so that they don't overlap other nodes
-    rslightdome.moveToGoodPosition()
-    rslight.moveToGoodPosition()
-
-
-#########################################################
-# Function to create camera node
-def create_cameraNode():
-
-    # Access obj network
-    obj = hou.node("/obj")
-
-    # Create cam node in obj network
-    cam = obj.createNode("cam", "cam_1080")
-
-    # Also, move it so that it doesn't overlap other nodes
-    cam.moveToGoodPosition()
-
-    # Define resolution using dictionary format
-    res = {'resx': 1920, 'resy': 1080}
-    # Apply resolution to camera
-    cam.setParms(res)
-
-    # Turn off camera node display & select flags
-    cam.setDisplayFlag(False)
-    cam.setSelectableInViewport(False)
-
-    # Set color for node to dark blue
-    cam.setColor(hou.Color(0.094, 0.369, 0.69))
-
-
-#########################################################
-# Function to create Redshift render ROP & IPR nodes
-def create_redshiftNode():
-
-    # Access out network
-    out = hou.node("/out")
-
-    # Create Redshift ROP in out network
-    redRop = out.createNode("Redshift_ROP")
-
-    # Set ROP gamma to No Gamma
-    redRop.setParms({"RS_gammaFileMode": "noGamma"})
-
-    #########################################
-    # Create Redshift IPR in out network
-    redIPR = out.createNode("Redshift_IPR")
-
-    # Also, move it so that it doesn't overlap other nodes
-    redIPR.moveToGoodPosition()
-
-
-#########################################################
-# Function to create blank geo node
-def create_matNode():
-
-    # Access shop network
-    shop = hou.node("/shop")
-
-    # Create Redshift material node
-    rsmat = shop.createNode("RS_Material", node_name="rsMat")
-
-    # Set color for node to dark red
-    rsmat.setColor(hou.Color(0.8, 0.016, 0.016))
-
-    # Set mat node to the preset of Plastic, which has a
-    # token of 2, hence the option identified
-    rsmat.setParms({"preset": "2"})
-
-
-#########################################################
-# Function to create node organization
-# Includes network boxes and sticky notes
-def create_networkGeo():
-
-    # Access obj network
-    obj = hou.node("/obj")
 
     # Create network box in obj network
     geonet = obj.createNetworkBox()
@@ -143,33 +35,211 @@ def create_networkGeo():
     geonet.setComment("GEONET")
     geonet.setColor(hou.Color(0.302, 0.525, 0.114))
 
-    # Collect all geo nodes in obj network
-    geo_node = obj.recursiveGlob("geo_*")
-    # The above returns tuple of single element
-    # Position single element to be within network box
-    geo_node[0].setPosition([0.5, 6])
+    geo_new_node.setPosition([0.5, 6])
     # Add geo node to network box
-    geonet.addItem(geo_node[0])
+    geonet.addItem(geo_new_node)
+
+
+#########################################################
+# Function to create Redshift light dome & single light nodes
+def create_lightNodeNet():
+
+    # Access obj network
+    obj = hou.node("/obj")
+
+    #########################################
+    # Create Redshift light dome node
+    rslightdome_node = obj.createNode("rslightdome", node_name="rsLightDome")
+
+    #########################################
+    # Create Redshift area light node
+    rslight_node = obj.createNode("rslight", node_name="rsLight")
+
+    #########################################
+    # Set parameters
+    # Disable area light
+    rslight_node.setParms({"light_enabled": "0"})
+    # Add path to default Houdini HDRI in light dome
+    rslightdome_node.setParms({"env_map": "$HFS/houdini/pic/hdri/HDRIHaven_kiara_5_noon_2k.rat"})
+    # Disable HDRI background in light dome
+    rslightdome_node.setParms({"background_enable": "0"})
+
+    #########################################
+
+    # Turn off light nodes display & select flags
+    rslightdome_node.setDisplayFlag(False)
+    rslight_node.setDisplayFlag(False)
+    rslightdome_node.setSelectableInViewport(False)
+    rslight_node.setSelectableInViewport(False)
+
+    # Set color for nodes to dark yellow
+    rslightdome_node.setColor(hou.Color(1.0, 0.725, 0.0))
+    rslight_node.setColor(hou.Color(1.0, 0.725, 0.0))
+
+    # Move them so that they don't overlap other nodes
+    rslightdome_node.setPosition([5.5, 6])
+    rslight_node.setPosition([5.5, 5])
+
+    #########################################
+
+    # Create network box in obj network
+    lightnet = obj.createNetworkBox()
+
+    # Set network box size & position
+    lightnet.setSize([4,3])
+    lightnet.setPosition([5,4])
+
+    # Set network box label & color
+    lightnet.setComment("LIGHTNET")
+    lightnet.setColor(hou.Color(1.0, 0.725, 0.0))
+
+    # Add geo node to network box
+    lightnet.addItem(rslightdome_node)
+    lightnet.addItem(rslight_node)
+
+
+#########################################################
+# Function to create camera node
+def create_cameraNodeNet():
+
+    # Access obj network
+    obj = hou.node("/obj")
+
+    # Create cam node in obj network
+    cam_node = obj.createNode("cam", "cam_1080")
+
+    # Also, move it so that it doesn't overlap other nodes
+    #cam.moveToGoodPosition()
+    cam_node.setPosition([10.5, 6])
+
+    # Define resolution using dictionary format
+    res = {'resx': 1920, 'resy': 1080}
+    # Apply resolution to camera
+    cam_node.setParms(res)
+
+    # Turn off camera node display & select flags
+    cam_node.setDisplayFlag(False)
+    cam_node.setSelectableInViewport(False)
+
+    # Set color for node to dark blue
+    cam_node.setColor(hou.Color(0.094, 0.369, 0.69))
+
+    #########################################
+
+    # Create network box in obj network
+    camnet = obj.createNetworkBox()
+
+    # Set network box size & position
+    camnet.setSize([4,3])
+    camnet.setPosition([10,4])
+
+    # Set network box label & color
+    camnet.setComment("CAMNET")
+    camnet.setColor(hou.Color(0.094, 0.369, 0.69))
+
+    # Add geo node to network box
+    camnet.addItem(cam_node)
+
+
+#########################################################
+# Function to create Redshift render ROP & IPR nodes
+def create_redshiftNodeNet():
+
+    # Access out network
+    out = hou.node("/out")
+
+    # Create Redshift ROP in out network
+    redRop = out.createNode("Redshift_ROP")
+
+    #########################################
+    # Set parameters
+    # Set render camera to main camera
+    redRop.setParms({"RS_renderCamera": "/obj/cam_1080"})
+    # Set to No Gamma
+    redRop.setParms({"RS_gammaFileMode": "noGamma"})
+    # Set render camera to main camera
+    redRop.setParms({"PrimaryGIEngine": "RS_GIENGINE_BRUTE_FORCE"})
+
+    #########################################
+    # Create Redshift IPR in out network
+    redIPR = out.createNode("Redshift_IPR")
+
+    # Set color for node to dark red
+    redRop.setColor(hou.Color(0.8, 0.016, 0.016))
+    redIPR.setColor(hou.Color(0.8, 0.016, 0.016))
+
+    # Also, move it so that it doesn't overlap other nodes
+    #redIPR.moveToGoodPosition()
+
+    # Position nodes
+    redIPR.setPosition([0, 2])
+    redRop.setPosition([0, 4])
+
+
+#########################################################
+# Function to create blank geo node
+def create_matNodeNet():
+
+    # Access shop network
+    shop = hou.node("/shop")
+
+    # Create Redshift material node
+    rsMatReflect = shop.createNode("RS_Material", node_name="rsMat_Reflect")
+    rsMatMatte = shop.createNode("RS_Material", node_name="rsMat_Matte")
+    rsMatCd = shop.createNode("redshift_vopnet", node_name="rsMat_Cd")
+
+    # Set color for node to dark red
+    rsMatReflect.setColor(hou.Color(0.8, 0.016, 0.016))
+    rsMatMatte.setColor(hou.Color(0.8, 0.016, 0.016))
+    rsMatCd.setColor(hou.Color(0.8, 0.016, 0.016))
+
+    # Set reflect mat node to the preset of Plastic, which has a
+    # token of 2, hence the option identified
+    # Matte mat node set to 12, paper
+    rsMatReflect.setParms({"preset": "2"})
+    rsMatMatte.setParms({"preset": "12"})
+
+    # Position nodes
+    rsMatReflect.setPosition([0, 2])
+    rsMatMatte.setPosition([0, 4])
+    rsMatCd.setPosition([0, 6])
+
+    #########################################
+    # Access inside rsMat_Cd
+    rsMatCdVex = hou.node("shop/rsMat_Cd")
+
+    rsMatCd_mat = rsMatCdVex.createNode("redshift::Material", node_name="Material_Comp")
+    rsMatCd_cd = rsMatCdVex.createNode("redshift::ParticleAttributeLookup", node_name="Pt_Attribute")
+
+    #########################################
+    # Position nodes
+    rsMatCd_mat.setPosition([-3, 0])
+    rsMatCd_cd.setPosition([-6, 0])
+
+    #########################################
+    # Set parameters and network connections
+    # Set @Cd attribute for Pt_Attribute
+    rsMatCd_cd.setParms({"attribute": "Cd"})
+    # Connect Pt_Attribute to Diffuse input in Material_Comp
+    rsMatCd_mat.setInput(0, rsMatCd_cd, 0)
+
+    # Assign existing redshift material out node to variable rsMatOut
+    rsMatOut = rsMatCdVex.node('redshift_material1')
+    # Connect Material_Comp to Surface in redshift_material
+    rsMatOut.setInput(0, rsMatCd_mat, 0)
+
 
 #########################################################
 # Collect functions to generate all new nodes at startup
 # in new main() function
 def main():
-    create_geoNode()
-    create_cameraNode()
-    create_redshiftNode()
-    create_lightNodes()
-    create_matNode()
+    # content creation
+    create_geoNodeNet()
+    create_cameraNodeNet()
+    create_redshiftNodeNet()
+    create_lightNodeNet()
+    create_matNodeNet()
 
-    #project management
-    create_networkGeo()
-
-    # lightNodes() function deactivated in 123.py file
-    # since the lightNodes() function in the 456.py file
-    # appears to duplicate them, so the 456.py file
-    # lightNodes() function takes precedence and this
-    # one's is deactivated:
-    # create_lightNodes()
 
 #########################################################
 # Call main function
