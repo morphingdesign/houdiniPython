6efe88279b08c2ed1e15d511899af3e0bc9c4244
Hans Palacios <architect.hp@gmail.com>
2019-09-03 15:29:47 -0500

    Update matnet nodes and add EOF for geo and rndrnets.

--- 123.py
+++ 123.py
@@ -77,6 +77,13 @@ def create_geoNodeNet():
     # Add geo node to network box
     geonet.addItem(geo_new_node)
 
+    #########################################
+
+    # Create null EOF node within, color & enable display
+    geonet_EOF = geo_new_node.createNode("null", node_name="OUT")
+    geonet_EOF.setColor(hou.Color(0.8, 0.016, 0.016))
+    geonet_EOF.setDisplayFlag(True)
+
 
 #########################################################
 # Function to create blank dopnet node
@@ -156,6 +163,27 @@ def create_rndrNodeNet():
     # Add geo node to network box
     rndrgeonet.addItem(rndrgeo_new_node)
 
+    #########################################
+
+    # Create null EOF node within, color & enable display
+    rndrnet_eof = rndrgeo_new_node.createNode("null", node_name="OUT")
+    rndrnet_eof.setColor(hou.Color(0.8, 0.016, 0.016))
+    rndrnet_eof.setDisplayFlag(True)
+
+    # Create object merge node within & color
+    rndrnet_om = rndrgeo_new_node.createNode("object_merge", node_name="get_geo")
+    rndrnet_om.setColor(hou.Color(0.302, 0.525, 0.114))
+
+    # Link OM obj with EOF in GEONET geo obj
+    rndrnet_om.setParms({"objpath1": "/obj/geo_newGeo/OUT"})
+
+    # Set OM transform to 'Into This Object' token
+    rndrnet_om.setParms({"xformtype": "local"})
+
+    # Link OM to EOF, & organize nodes in RNDRNET
+    rndrnet_eof.setInput(0, rndrnet_om, 0)
+    rndrgeo_new_node.layoutChildren()
+
 
 #########################################################
 # Function to create Redshift light dome & single light nodes
@@ -225,14 +253,30 @@ def create_cameraNodeNet():
     # Create cam node in obj network
     cam_node = obj.createNode("cam", "cam_1080")
 
-    # Also, move it so that it doesn't overlap other nodes
-    #cam.moveToGoodPosition()
-    cam_node.setPosition([25.5, 6])
-
+    #########################################
+    # Set parameters
     # Define resolution using dictionary format
     res = {'resx': 1920, 'resy': 1080}
     # Apply resolution to camera
     cam_node.setParms(res)
+
+    # Position camera for viewing scene
+    cam_node.setParms({"ty": "0.5"})
+    cam_node.setParms({"tz": "5.0"})
+
+    # Enable DOF & link cam focus distance to RS cam
+    cam_node.setParms({"RS_campro_dofEnable": "1"})
+    # Disable Houdini cam DOF link
+    cam_node.setParms({"RS_campro_dofUseHoudiniCamera": "0"})
+    # Adjust DOF power to be lower
+    cam_node.setParms({"RS_campro_dofPower": "0.001"})
+    # Set Houdini cam focus distance to variable
+    focus = hou.parm('/obj/cam_1080/focus')
+    # Use focus distance variable as channel reference in RS cam
+    cam_node.setParms({"RS_campro_dofDistance": focus})
+
+
+    #########################################
 
     # Turn off camera node display & select flags
     cam_node.setDisplayFlag(False)
@@ -240,6 +284,10 @@ def create_cameraNodeNet():
 
     # Set color for node to dark blue
     cam_node.setColor(hou.Color(0.094, 0.369, 0.69))
+
+    # Also, move it so that it doesn't overlap other nodes
+    # cam.moveToGoodPosition()
+    cam_node.setPosition([25.5, 6])
 
     #########################################
 
@@ -363,40 +411,72 @@ def create_matNodeNet():
     rsMat_Cd_Reflect.setColor(hou.Color(0.8, 0.016, 0.016))
     rsMat_Glass.setColor(hou.Color(0.8, 0.016, 0.016))
 
-    # Set reflect mat node to the preset of Plastic, which has a
-    # token of 2, hence the option identified
-    # Matte mat node set to 12, paper
-    #rsMatReflect.setParms({"preset": "2"})
-    #rsMatMatte.setParms({"preset": "12"})
-
     # Position nodes
     rsMat_Cd_Matte.setPosition([0, 2])
     rsMat_Cd_Reflect.setPosition([0, 4])
     rsMat_Glass.setPosition([0, 6])
 
     #########################################
-    # Access inside rsMat_Cd
-    #rsMatCdVex = hou.node("shop/rsMat_Cd")
+    # Access inside rsMat_Cd_Matte to create nodes
+    cdmatte_mat = rsMat_Cd_Matte.createNode("redshift::Material", node_name="Material_Comp")
+    cdmatte_cd = rsMat_Cd_Matte.createNode("redshift::ParticleAttributeLookup", node_name="Pt_Attribute")
 
-    #rsMatCd_mat = rsMatCdVex.createNode("redshift::Material", node_name="Material_Comp")
-    #rsMatCd_cd = rsMatCdVex.createNode("redshift::ParticleAttributeLookup", node_name="Pt_Attribute")
-
-    #########################################
     # Position nodes
-    #rsMatCd_mat.setPosition([-3, 0])
-    #rsMatCd_cd.setPosition([-6, 0])
+    cdmatte_mat.setPosition([-3, 0])
+    cdmatte_cd.setPosition([-6, 0])
 
-    #########################################
     # Set parameters and network connections
     # Set @Cd attribute for Pt_Attribute
-    #rsMatCd_cd.setParms({"attribute": "Cd"})
+    cdmatte_cd.setParms({"attribute": "Cd"})
     # Connect Pt_Attribute to Diffuse input in Material_Comp
-    #rsMatCd_mat.setInput(0, rsMatCd_cd, 0)
+    cdmatte_mat.setInput(0, cdmatte_cd, 0)
 
-    # Assign existing redshift material out node to variable rsMatOut
-    #rsMatOut = rsMatCdVex.node('redshift_material1')
+    # Matte mat node set to 12, paper
+    cdmatte_mat.setParms({"preset":"12"})
+
+    # Assign existing redshift material out node to variable
+    cdmatte_out = rsMat_Cd_Matte.node('redshift_material1')
     # Connect Material_Comp to Surface in redshift_material
-    #rsMatOut.setInput(0, rsMatCd_mat, 0)
+    cdmatte_out.setInput(0, cdmatte_mat, 0)
+
+    #########################################
+    # Access inside rsMat_Cd_Reflect to create nodes
+    cdreflect_mat = rsMat_Cd_Reflect.createNode("redshift::Material", node_name="Material_Comp")
+    cdreflect_cd = rsMat_Cd_Reflect.createNode("redshift::ParticleAttributeLookup", node_name="Pt_Attribute")
+
+    # Position nodes
+    cdreflect_mat.setPosition([-3, 0])
+    cdreflect_cd.setPosition([-6, 0])
+
+    # Set parameters and network connections
+    # Set @Cd attribute for Pt_Attribute
+    cdreflect_cd.setParms({"attribute": "Cd"})
+    # Connect Pt_Attribute to Diffuse input in Material_Comp
+    cdreflect_mat.setInput(0, cdreflect_cd, 0)
+
+    # Mat node set to 2, plastic
+    cdreflect_mat.setParms({"preset":"2"})
+
+    # Assign existing redshift material out node to variable
+    cdreflect_out = rsMat_Cd_Reflect.node('redshift_material1')
+    # Connect Material_Comp to Surface in redshift_material
+    cdreflect_out.setInput(0, cdreflect_mat, 0)
+
+    #########################################
+    # Access inside rsMat_Cd_Glass to create node
+    glass_mat = rsMat_Glass.createNode("redshift::Material", node_name="Material_Comp")
+
+    # Position node
+    glass_mat.setPosition([-3, 0])
+
+    # Set parameters and network connections
+    # Mat node set to 0, glass
+    glass_mat.setParms({"preset":"0"})
+
+    # Assign existing redshift material out node to variable
+    glass_out = rsMat_Glass.node('redshift_material1')
+    # Connect Material_Comp to Surface in redshift_material
+    glass_out.setInput(0, glass_mat, 0)
 
 
 #########################################################
