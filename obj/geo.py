28011d2762dc3ef0db93b75775cfaf23fe881114
morphingdesign <architect.hp@gmail.com>
2020-07-27 14:27:18 -0500

    Test access to individual nodes at obj and sop levels for customizing default parameters. Test successful. Note the folder and file naming convention used; ref link in source.

---
+++ obj/geo.py
@@ -0,0 +1,8 @@
+#########################################################
+"""Custom script to access geo object in obj network."""
+
+# Source: https://www.sidefx.com/docs/houdini/hom/locations.html
+
+# Output test
+print "hello"
+print kwargs['node']
---
+++ sop/labs-rizomuv_optimize_OnCreated.py
@@ -0,0 +1,11 @@
+#########################################################
+"""Custom script to define Rizom.exe path on disk."""
+
+# Source: https://www.sidefx.com/docs/houdini/hom/locations.html
+
+# Set custom file path for .exe
+# riz.setParms({"rizomloc": "C:/Program Files/Rizom Lab/RizomUV 2020/rizomuv.exe"})
+
+# Output test
+print "hello"
+print kwargs['node']
