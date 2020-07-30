cf4ccb38f7633e37bf63ab5fadbc4654bb6837eb
morphingdesign <architect.hp@gmail.com>
2020-07-30 15:31:21 -0500

    Default group parameter name to operator name.

--- common_py_lib/utils.py
+++ common_py_lib/utils.py
@@ -23,3 +23,8 @@ def lib_create_matNode(mat, name, color, pos):
 def lib_set_rizomExe(sop_name):
     """Set path to Rizom.exe"""
     sop_name.parm("rizomloc").set("C:/Program Files/Rizom Lab/RizomUV 2020/rizomuv.exe")
+
+# Function to set parameter name to operator name
+def lib_set_opname(sop_name, parm_name):
+    """Set param name to op name"""
+    sop_name.parm(parm_name).set("`opname('.')`")
