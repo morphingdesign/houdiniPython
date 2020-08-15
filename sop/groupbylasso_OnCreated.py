ab5b33022a62d2a4c52bf303e27a831e5b220498
morphingdesign <architect.hp@gmail.com>
2020-08-15 13:12:40 -0500

    Add operator name to group by lasso sop.

---
+++ sop/groupbylasso_OnCreated.py
@@ -0,0 +1,20 @@
+#########################################################
+"""Custom script to setup typical group expression operator name."""
+
+import sys
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# IMPORTANT: Reset path to user-defined path with 'username'
+library_path = r'C:\Users\username\Documents\houdini18.0\scripts'
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+sys.path.append(library_path)
+from common_py_lib import utils
+
+# Access this node via variable
+node = kwargs['node']
+
+# Access function from utils lib
+utils.lib_set_opname(node, "group")
