2f832846f2c47115689b104b724342c872824350
Hans Palacios <architect.hp@gmail.com>
2020-12-04 23:10:12 -0600

    Add on created parameters for box and sphere geo.

---
+++ sop/sphere_OnCreated.py
@@ -0,0 +1,24 @@
+#########################################################
+"""Custom script to setup typical sphere startup parameters."""
+
+import sys
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# IMPORTANT: Reset path to user-defined path with 'username'
+library_path = r'C:\Users\username\Documents\houdini18.0\scripts'
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+sys.path.append(library_path)
+from common_py_lib import utils
+
+# Access this node via variable
+node = kwargs['node']
+
+# Set Parameters
+
+# Set primitive type to polygon mesh.
+node.parm("type").set("polymesh")
+
+node.parmTuple('rad').set([0.5,0.5,0.5])
