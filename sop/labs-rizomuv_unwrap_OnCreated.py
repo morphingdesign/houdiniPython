77282cd00ed202a0396c852dd9f6105a29fec4b8
Hans Palacios <architect.hp@gmail.com>
2020-12-07 13:07:54 -0600

    Update docstrings.

--- sop/labs-rizomuv_unwrap_OnCreated.py
+++ sop/labs-rizomuv_unwrap_OnCreated.py
@@ -1,23 +1,27 @@
-#########################################################
-"""Custom script to define Rizom.exe path on disk."""
+# -----------------------------------------------------------
+# labs-rizomuv_unwrap_OnCreated.py
+# v.1.1
+# Updated: 20201207
+# ----------------------------------------------------------
 
-# Source: https://www.sidefx.com/docs/houdini/hom/locations.html
-
-import sys
-
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# IMPORTANT: Reset path to user-defined path with 'username'
-library_path = r'C:\Users\username\Documents\houdini18.0\scripts'
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-sys.path.append(library_path)
+"""
+Custom script to define Rizom.exe path on disk
+"""
 from common_py_lib import utils
 
-# Access this node via variable
+# -----------------------------------------------------------
+# GLOBAL VARIABLES )))))))))))))))))))))))))))))))))))) START
+# -----------------------------------------------------------
 node = kwargs['node']
+# -----------------------------------------------------------
+# GLOBAL VARIABLES )))))))))))))))))))))))))))))))))))))) END
+# -----------------------------------------------------------
 
-# Access function from utils lib
-# Set path to Rizom.exe on disk
+# -----------------------------------------------------------
+# CUSTOMIZATIONS )))))))))))))))))))))))))))))))))))))) START
+# -----------------------------------------------------------
+# Access function from utils lib to set path to Rizom.exe on disk
 utils.lib_set_rizomExe(node)
+# -----------------------------------------------------------
+# CUSTOMIZATIONS )))))))))))))))))))))))))))))))))}}))))) END
+# -----------------------------------------------------------
