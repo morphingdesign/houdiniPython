58460808bdd54e440c1959929a8af496f549e98d
Hans Palacios <architect.hp@gmail.com>
2021-02-06 21:35:50 -0600

    Updated to resolve name of newly creating Object Merge nodes. Previous version kept new nodes after first node as 'Object Merge'. Script updated to configure new name by searching network for potential existing matches and appending name with counter number.

--- shelf/houObjMergeAdvNode.py
+++ shelf/houObjMergeAdvNode.py
@@ -1,6 +1,6 @@
 # -----------------------------------------------------------
 # houObjMergeAdvNode.py
-# v.1.0
+# v.1.1
 # Updated: 20210206
 # ----------------------------------------------------------
 
@@ -12,6 +12,9 @@ originally copied node/s. By default, operation nests
 reference path as absolute, but tool includes kwargs for CTRL
 to specify path as relative.
 """
+
+from nodesearch import parser   # For parsing node matches
+import toolutils                # Collect active pane/network
 
 # Check if node/s are selected by user; if not, NOP.
 if hou.selectedNodes():
@@ -49,9 +52,28 @@ if hou.selectedNodes():
         # Set to relative path.
         objMerge.parm("objpath1").set("../" + selectedNode.name())
 
-    # Set name for newly created Object Merge node.
-    if parent.node("./get_%s" % selectedNode.name()) == None:
-        objMerge.setName("get_%s" % selectedNode.name())
+    # Set name for new Object Merge node and mitigate existing
+    # name matches.
+    #
+    # Specify active pane and network.
+    active_pane = toolutils.activePane(kwargs)
+    network = active_pane.pwd()
+    # Initialize counter for collecting matches.
+    counter = 0;
+    # Search for existing nodes with same name using matcher
+    # object and node parsing operation
+    searchStringPrefix = "get_%s" % selectedNode.name()
+    matcher = parser.parse_query(searchStringPrefix)
+    # Search through all nodes in active network for existing
+    # matches and increase counter for each match.
+    for node in matcher.nodes(network):
+        if matcher.matches(node):
+            counter += 1
+    # Set name for newly created Object Merge node, and append
+    # with last counter number. This allows for all new Object
+    # Merges to preserve the name of the node from which they
+    # are linked, while only differing by the suffix number.
+    objMerge.setName("get_%s_%d" % (selectedNode.name(), counter))
     # -----------------------------------------------------------
     # CONFIGURE PARAMETERS )))))))))))))))))))))))))))))))))) END
     # -----------------------------------------------------------
