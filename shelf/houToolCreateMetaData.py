a7d752b77a329edd175906a08019f60edb34f4f1
Hans Palacios <architect.hp@gmail.com>
2021-11-09 12:04:51 -0600

    Add tool for storing hip metadata to selected nodes.

---
+++ shelf/houToolCreateMetaData.py
@@ -0,0 +1,41 @@
+# -----------------------------------------------------------
+# houToolCreateMetaData.py
+# v.1.0
+# Updated: 20211109
+# ----------------------------------------------------------
+
+"""
+Shelf Tool Name: Set Creator
+Custom shelf tool to store creation metadata to selected node/s
+in its built-in creatorState.
+"""
+
+import hou
+
+# Modify creator metadata for selected nodes in this project file.
+# Attribute the hipname.
+
+# -----------------------------------------------------------
+# LOCAL VARIABLES ))))))))))))))))))))))))))))))))))))) START
+# -----------------------------------------------------------
+
+hipname = hou.expandString("$HIPNAME")
+hipfile = hou.expandString("$HIPFILE")
+nodes = hou.selectedNodes()
+
+# -----------------------------------------------------------
+# LOCAL VARIABLES ))))))))))))))))))))))))))))))))))))))) END
+# -----------------------------------------------------------
+
+# -----------------------------------------------------------
+# SET ATTRIBUTE ))))))))))))))))))))))))))))))))))))))) START
+# -----------------------------------------------------------
+
+for node in nodes:
+    # NODE CREATOR STATE ****************************
+    node.setCreatorState(hipname)
+    node.setBuiltExplicitly(False)
+
+# -----------------------------------------------------------
+# SET ATTRIBUTE ))))))))))))))))))))))))))))))))))))))))) END
+# -----------------------------------------------------------
