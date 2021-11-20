55b803f83061b4962d0b2c225cad4d02fb99ac25
Hans Palacios <architect.hp@gmail.com>
2021-11-20 13:06:19 -0600

    Update with conditional to toggle connections among nodes within HDA.

--- common_py_lib/hdaUtils.py
+++ common_py_lib/hdaUtils.py
@@ -1,7 +1,7 @@
 # -----------------------------------------------------------
 # hdaUtils.py
 # v.1.0
-# Updated: 20211119
+# Updated: 20211120
 # Houdini version: 18.5.563, Python 3
 # Redshift version: 3.0.46
 # -----------------------------------------------------------
@@ -46,11 +46,23 @@ def toggleOutput(parmName):
     nullNode = node.item("OUT_%s" % outputName)
     suboutNode = node.item("suboutput")
 
+    # Evaluate number of nodes attached to current node's outputs.
+    outputState = len(nullNode.outputs())
+
+    # If greater than 0, then node is connected to suboutNode.
+    if outputState > 0:
+        # Detach nullNode from suboutNode.
+        suboutNode.setInput(index, None, 0)
+    else:
+        # If not connected, then connect nullNode to corresponding index
+        # outlet on suboutNode.
+        suboutNode.setInput(index, nullNode, 0)
+
     # Debug log
     print("***************************************")
-    print("[%d]ParmName: %s" % (index, outputName))
-    print("ParmValue: %s" % parmValue)
-    print("NullNode: %s" % nullNode)
+    # print("[%d]ParmName: %s" % (index, outputName))
+    # print("ParmValue: %s" % parmValue)
+    print("NullNode: %s; num of outputs: %d" % (nullNode, outputState))
 
 # -----------------------------------------------------------
 # TOGGLE CUSTOM OUTPUTS ))))))))))))))))))))))))))))))))) END
